50234df6442b84eedd7af0e34d009952935cc118
thekaigonzalez <gkai70264@gmail.com>
2021-07-06 14:15:33 -0400

    initial commit

---
+++ SymSearchCSEdition/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SymSearchCSEdition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class Search (Symbol Searcher in C#)!");
+            Console.WriteLine("Type the name of a DLL Below.");
+            string dll = Console.ReadLine();
+            Assembly assembly = Assembly.LoadFrom(dll);
+
+            if (Directory.Exists(dll))
+            {
+                while (true)
+                {
+                    Console.WriteLine("Type the name of a DLL class below. (example: System.Runtime OR System.Linq)");
+                    Console.Write(">>> ");
+                    string classs = Console.ReadLine();
+                    Type type = assembly.GetType(classs);
+                    object instance = Activator.CreateInstance(type); //creates an instance of that class
+                    MethodInfo[] methods = type.GetMethods();
+                    if (classs == "methods-available")
+                    {
+                        Console.WriteLine(methods);
+                    }
+                    else
+                    {
+                        foreach (object OBJ in methods)
+                        {
+                            Console.WriteLine(OBJ.ToString());
+                            if (OBJ.ToString() == "")
+                            {
+                                object result = methods[0].Invoke(instance, new object[] { });
+                            }
+                            
+                        }
+                        
+                    }
+
+                }
+            }
+        }
+    }
+}
